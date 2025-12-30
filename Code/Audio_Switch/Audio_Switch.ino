15e139a342ce0eecd1246118dd6acc7998dab440
Justin Nguyen <iamaalt921@gmail.com>
2025-12-30 14:08:45 -0700

    Addded a delay to reduce hysteresis

--- Code/Audio_Switch/Audio_Switch.ino
+++ Code/Audio_Switch/Audio_Switch.ino
@@ -1,18 +1,18 @@
 const int audioPin = A0;
 const int switchPinA = 2; // Chua 1
 const int switchPinB = 3; // Chua 2
-const int threshold = 768; // THRESHOLD FOR MAX VOLUME ON WINDOWS (100) AND MAX VOLUME ON MUSIC PLAYER
+const int threshold = 768; // THRESHOLD DEPENDS ON AUDIO FILE AND WINDOWS/MUSIC PLAYER VOLUME
 
 void setup() {
   Serial.begin(9600);
   pinMode(switchPinA, OUTPUT);
-  pinMode(switchPinB, OUTPUT);
+  pinMode(switchPinB, OUTPUT); 
 }
 
 void loop() {
   int level = analogRead(audioPin);
   Serial.println(level);
-
+  delay(500);
   if (level > threshold) {
       Serial.println("SWITCHED HIGH");
       digitalWrite(switchPinA, HIGH);
